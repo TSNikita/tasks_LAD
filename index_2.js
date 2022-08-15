dc90d7892630db4c4062380da9a13214967f4710
Nikita Tsipelev <npometall@mail.ru>
2022-08-15 12:46:48 +0300

    fix task_2

---
+++ index_2.js
@@ -0,0 +1,16 @@
+//Задача 2
+// https://jsbin.com/deyoteb/edit?html,css,js,output - исправить код таким образом,
+// чтобы при фокусе у инпутов добавлялась красная рамка.
+// Обработка событий должна происходить на formElement.
+
+
+var formElement = document.getElementById('formElement');
+
+formElement.addEventListener("focus", function(event) {
+    event.target.classList.add('focused');
+}, true);
+
+
+formElement.addEventListener("blur", function(event) {
+    event.target.classList.remove('focused');
+}, true);
